a993cd245c6b384391be302c2cb2625e149a4107
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-23 19:29:24 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -16,7 +16,7 @@ class Employee {
 
     // Add method to calculate employee annual salary
     calculateAnnualSalary() {
-        return this.salary * 12
+        return this.salary * 12;
     }
 };
 
@@ -25,3 +25,29 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 
 console.log(emp1.getDetails());
 console.log(emp1.calculateAnnualSalary()); 
+
+// TASK 2: CREATING A MANAGER CLASS
+
+// Create manager class that extends employee class
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+    
+    // Override getDetails() method to include team size
+    getDetails() {
+        return super.getDetails() + `, Team Size: ${this.teamSize}`;
+    }
+
+    // Add method to calculate 10% of manager's annual salary 
+    calculateBonus() {
+        return this.calculateAnnualSalary(this.salary) * 0.1;
+    }
+}
+
+// Test Case
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
